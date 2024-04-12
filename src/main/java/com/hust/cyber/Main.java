90654d764b1e86ee52681d599dd6aa5c561fcaa9
bananaplayctf <bananaplayctf@gmail.com>
2024-04-12 12:38:01 +0700

    1

--- src/main/java/com/hust/cyber/Main.java
+++ src/main/java/com/hust/cyber/Main.java
@@ -3,5 +3,6 @@ package com.hust.cyber;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("cayvcl");
     }
 }
