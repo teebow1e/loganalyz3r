6b90905c93522a81c909cf54831ba75eda76377b
bananaplayctf <bananaplayctf@gmail.com>
2024-04-12 12:50:47 +0700

    test branch

--- src/main/java/com/hust/cyber/Main.java
+++ src/main/java/com/hust/cyber/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello Cyber!");
+        System.out.println("xin chao");
     }
 }
