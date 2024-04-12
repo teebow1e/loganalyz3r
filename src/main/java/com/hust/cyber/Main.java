e81e56091b7c3905fdb4512ab86f993c56d9229e
bananaplayctf <bananaplayctf@gmail.com>
2024-04-12 13:50:22 +0700

    test private repo

--- src/main/java/com/hust/cyber/Main.java
+++ src/main/java/com/hust/cyber/Main.java
@@ -5,7 +5,7 @@ import javafx.application.Application;
 public class Main {
     public static void main(String[] args) {
         // Launch the JavaFX application by calling the launch method
-        System.out.println("HELLO");
+        System.out.println("HELLO1");
         Application.launch(LoginForm.class, args);
     }
 }
