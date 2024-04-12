78c8b578916c6fd2d06f928e242b2807a2f6057a
bananaplayctf <bananaplayctf@gmail.com>
2024-04-12 13:37:24 +0700

    Change Main.java

--- src/main/java/com/hust/cyber/Main.java
+++ src/main/java/com/hust/cyber/Main.java
@@ -1,10 +1,11 @@
 package com.hust.cyber;
 
+import javafx.application.Application;
+
 public class Main {
     public static void main(String[] args) {
         // Launch the JavaFX application by calling the launch method
         System.out.println("HELLO");
-        LoginForm.launch(LoginForm.class, args);
-
+        Application.launch(LoginForm.class, args);
     }
 }
