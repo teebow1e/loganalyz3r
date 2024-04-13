4f50134db4c0a10d249fcb2eddd27f09fb907c60
Trung Tran <130235050+teebow1e@users.noreply.github.com>
2024-04-13 16:08:30 +0700

    Update Main.java

--- src/main/java/com/hust/cyber/Main.java
+++ src/main/java/com/hust/cyber/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println('aaaaa");
         Application.launch(LoginForm.class, args);
     }
 }
