b1f7668d380068f9854c0fbd616db63ae4c1df18
bananaplayctf <bananaplayctf@gmail.com>
2024-04-14 15:33:12 +0700

    fix Main.java

--- src/main/java/com/hust/cyber/Main.java
+++ src/main/java/com/hust/cyber/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println('aaaaa");
         Application.launch(LoginForm.class, args);
     }
 }
