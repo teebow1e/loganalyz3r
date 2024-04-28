009a22f3d2d4996e1769af2d1a62c1e7a7aadb36
bananaplayctf <bananaplayctf@gmail.com>
2024-04-28 11:53:36 +0700

    refactor(Fixing-UI-for-table-and-adding-column-for-checkbox): Dynamically create table base on the headers of the csv, and manually create extra column for checkbox. Along with create extra column for checkbox

--- src/main/java/dataanalyzer/TableVisualizer.java
+++ src/main/java/dataanalyzer/TableVisualizer.java
@@ -1,11 +1,14 @@
 package dataanalyzer;
 
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Parent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
@@ -19,19 +22,27 @@ public class TableVisualizer {
         TableView<String[]> tableView = (TableView<String[]>) mainVBox.lookup("#Table");
 
         tableView.getItems().clear();
+        tableView.getColumns().clear();
 
-        if (!data.isEmpty()) {
-            // Add columns dynamically
-            String[] headers = data.get(0).split(",");
-            for (int i = 0; i < headers.length; i++) {
-                final int columnIndex = i;
-                TableColumn<String[], String> column = new TableColumn<>(headers[i]);
-                column.setCellValueFactory(param -> new SimpleStringProperty(param.getValue()[columnIndex]));
-                column.setResizable(false);
-                column.getStyleClass().add("log-table-column-" + (i + 1));
-                tableView.getColumns().add(column);
-            }
+        String[] headers = data.get(0).split(",");
+        for (int i = 0; i < headers.length; i++) {
+            final int columnIndex = i;
+            TableColumn<String[], String> column = new TableColumn<>(headers[i]);
+            column.setCellValueFactory(param -> new SimpleStringProperty(param.getValue()[columnIndex]));
+            column.setResizable(false);
+            column.getStyleClass().add("log-table-column-" + (i + 1));
+            tableView.getColumns().add(column);
         }
+
+
+        // Add a checkbox column
+        TableColumn<String[], Boolean> checkboxColumn = new TableColumn<>("Checkbox");
+        checkboxColumn.setCellValueFactory(param -> new SimpleBooleanProperty(true)); // Replace with your data model property
+        checkboxColumn.setCellFactory(CheckBoxTableCell.forTableColumn(checkboxColumn));
+        checkboxColumn.getStyleClass().add("log-table-column-" + 9);
+        tableView.getColumns().add(checkboxColumn);
+
+
 
         ObservableList<String[]> rows = FXCollections.observableArrayList();
         for (int i = 1; i < data.size(); i++) {
