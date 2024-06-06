e5757d82fcf57088e7424e4bf852cd582fd1f036
teebow1e <tritrungtq@gmail.com>
2024-06-06 15:09:32 +0700

    feat(tailer): add stop mechanic for tailer

--- src/main/java/test/TailerTest.java
+++ src/main/java/test/TailerTest.java
@@ -6,6 +6,10 @@ import org.apache.commons.io.input.TailerListenerAdapter;
 import java.io.File;
 
 public class TailerTest {
+
+    // Define a flag to indicate whether the tailer should keep running
+    private static volatile boolean running = true;
+
     public static void main(String[] args) {
         File file = new File("logs/apache_nginx/access_log_100.log");
         System.out.println(file.length());
@@ -13,6 +17,13 @@ public class TailerTest {
         TailerListenerAdapter listener = new TailerListenerAdapter() {
             @Override
             public void handle(String line) {
+                // Check if the tailer should keep running
+                if (!running) {
+                    // If not, interrupt the thread
+                    System.out.println("the thread is instructed to stop");
+                    Thread.currentThread().interrupt();
+                    return;
+                }
                 System.out.println("handler called");
                 System.out.printf("new line added: %s\n", line);
             }
@@ -22,5 +33,22 @@ public class TailerTest {
         Thread thread = new Thread(tailer);
         System.out.println("tailer ready to run");
         thread.start();
+
+        // Wait for some time and then stop the tailer
+        try {
+            Thread.sleep(10000);
+            System.out.println("finished watiing");
+            // Wait for 5 seconds
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("terminate thread now");
+        thread.interrupt();
+
+        // Set the running flag to false to stop the tailer
+//        running = false;
+        tailer.stop();
     }
 }
+
