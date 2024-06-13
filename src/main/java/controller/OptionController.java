acb617f90f529c0ba2062b1ef830bbb7ad9e8665
teebow1e <tritrungtq@gmail.com>
2024-06-14 02:38:54 +0700

    feat(option): add UI boilerplate

--- src/main/java/controller/OptionController.java
+++ src/main/java/controller/OptionController.java
@@ -1,4 +1,44 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class OptionController {
+    @FXML
+    private TextField apacheLogLocationTF;
+
+    @FXML
+    private Button chooseApacheLocationBtn;
+
+    @FXML
+    private Button chooseModsecLocationBtn;
+
+    @FXML
+    private Button confirmApacheLocationBtn;
+
+    @FXML
+    private Button confirmModsecLocationBtn;
+
+    @FXML
+    private Button fetchUpdateDBBtn;
+
+    @FXML
+    private Text ipDBLocationText;
+
+    @FXML
+    private Text ipDBNewVerStatusText;
+
+    @FXML
+    private TextField modsecLogLocationTF;
+
+    @FXML
+    private Button updateDBBtn;
+
+    @FXML
+    void fetchUpdateDB(ActionEvent event) {
+        System.out.println("you want to fetch update");
+    }
 }
